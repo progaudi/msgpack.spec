2a752d7e95775c2e0ec070f23335d816b5ca4736
aensidhe <me@aensidhe.ru>
2018-07-28 02:31:21 +0300

    Binary test suite from https://github.com/kawanet/msgpack-test-suite/blob/master/src/12.binary.yaml

--- tests/msgpack.spec.tests/Reader/Binary.cs
+++ tests/msgpack.spec.tests/Reader/Binary.cs
@@ -6,8 +6,6 @@ namespace ProGaudi.MsgPack.Tests.Reader
     public class Binary
     {
         [Theory]
-        [InlineData(new byte[0], new byte[] {196, 0})]
-        [InlineData(new byte[] {1}, new byte[] {196, 1, 1})]
         [InlineData(new byte[]
         {
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
@@ -80,6 +78,16 @@ namespace ProGaudi.MsgPack.Tests.Reader
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0
         })]
+        // https://github.com/kawanet/msgpack-test-suite/blob/master/src/12.binary.yaml
+        [InlineData(new byte[0], new byte[] { 0xc4, 0x00 })]
+        [InlineData(new byte[0], new byte[] { 0xc5, 0x00, 0x00 })]
+        [InlineData(new byte[0], new byte[] { 0xc6, 0x00, 0x00, 0x00, 0x00 })]
+        [InlineData(new byte[] { 0x01 }, new byte[] { 0xc4, 0x01, 0x01 })]
+        [InlineData(new byte[] { 0x01 }, new byte[] { 0xc5, 0x00, 0x01, 0x01 })]
+        [InlineData(new byte[] { 0x01 }, new byte[] { 0xc6, 0x00, 0x00, 0x00, 0x01, 0x01 })]
+        [InlineData(new byte[] { 0x00, 0xff }, new byte[] { 0xc4, 0x02, 0x00, 0xff })]
+        [InlineData(new byte[] { 0x00, 0xff }, new byte[] { 0xc5, 0x00, 0x02, 0x00, 0xff })]
+        [InlineData(new byte[] { 0x00, 0xff }, new byte[] { 0xc6, 0x00, 0x00, 0x00, 0x02, 0x00, 0xff })]
         public void Test(byte[] value, byte[] data)
         {
             MsgPackSpec.TryReadBinary(data, out var owner, out var readSize).ShouldBeTrue();
--- tests/msgpack.spec.tests/Writer/Binary.cs
+++ tests/msgpack.spec.tests/Writer/Binary.cs
@@ -8,8 +8,6 @@ namespace ProGaudi.MsgPack.Tests.Writer
     public class Binary
     {
         [Theory]
-        [InlineData(new byte[0], new byte[] {196, 0})]
-        [InlineData(new byte[] {1}, new byte[] {196, 1, 1})]
         [InlineData(new byte[]
         {
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
@@ -82,6 +80,10 @@ namespace ProGaudi.MsgPack.Tests.Writer
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0
         })]
+        // https://github.com/kawanet/msgpack-test-suite/blob/master/src/12.binary.yaml
+        [InlineData(new byte[0], new byte[] { 0xc4, 0x00 })]
+        [InlineData(new byte[] { 0x01 }, new byte[] { 0xc4, 0x01, 0x01 })]
+        [InlineData(new byte[] { 0x00, 0xff }, new byte[] { 0xc4, 0x02, 0x00, 0xff })]
         public void Test(byte[] value, byte[] data)
         {
             var buffer = new Span<byte>(ArrayPool<byte>.Shared.Rent(data.Length));
