c91357462f62f2fc890caaa9bcd25e6dfa3432c0
aensidhe <me@aensidhe.ru>
2018-08-03 12:50:21 +0300

    Let's look on to Disassembly.

--- benchmarks/msgpack.spec.linux/NativeComparison.cs
+++ benchmarks/msgpack.spec.linux/NativeComparison.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Buffers;
-using System.Buffers.Binary;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using BenchmarkDotNet.Attributes;
 using ProGaudi.MsgPack;
 
 namespace msgpack.spec.linux
 {
     [MemoryDiagnoser]
+    [DisassemblyDiagnoser]
     [Q3Column]
     [MarkdownExporterAttribute.GitHub]
     public class NativeComparison
@@ -18,54 +16,25 @@ namespace msgpack.spec.linux
         private readonly byte[] _buffer = ArrayPool<byte>.Shared.Rent(short.MaxValue);
 
         [Benchmark]
-        public void MsgPackSpecArray()
+        public int MsgPackSpecArray()
         {
             var buffer = _buffer.AsSpan();
             var wroteSize = MsgPackSpec.WriteArray16Header(buffer, length);
             for (var i = 0u; i < length; i++)
                 wroteSize += MsgPackSpec.WriteUInt32(buffer.Slice(wroteSize), baseInt);
+
+            return wroteSize;
         }
 
         [Benchmark]
-        public void MsgPackSpecArrayMinus()
+        public int MsgPackSpecArrayMinus()
         {
             var buffer = _buffer.AsSpan();
             var wroteSize = MsgPackSpec.WriteArray16Header(buffer, length);
             for (var i = 0u; i < length; i++)
                 wroteSize += MsgPackSpec.WriteUInt32(buffer.Slice(wroteSize), baseInt - i);
-        }
 
-        [Benchmark(Baseline = true)]
-        public void CArray() => CNative.SerializeArray();
-
-        [Benchmark]
-        public void CArrayMinus() => CNative.SerializeArrayMinus();
-
-        [Benchmark]
-        public void CppArray() => CppNative.SerializeArray();
-
-        [Benchmark]
-        public void CppArrayMinus() => CppNative.SerializeArrayMinus();
-
-        private static class CNative
-        {
-            [DllImport("libcMsgPack.so", EntryPoint = "serializeIntArray", CallingConvention = CallingConvention.Cdecl)]
-            public static extern void SerializeArray();
-
-            [DllImport("libcMsgPack.so", EntryPoint = "serializeIntArrayMinus", CallingConvention = CallingConvention.Cdecl)]
-            public static extern void SerializeArrayMinus();
-
-            [DllImport("libcMsgPack.so", EntryPoint = "empty", CallingConvention = CallingConvention.Cdecl)]
-            public static extern void Empty();
-        }
-
-        private static class CppNative
-        {
-            [DllImport("libcppMsgPack.so", EntryPoint = "serializeIntArray", CallingConvention = CallingConvention.Cdecl)]
-            public static extern void SerializeArray();
-
-            [DllImport("libcMsgPack.so", EntryPoint = "serializeIntArrayMinus", CallingConvention = CallingConvention.Cdecl)]
-            public static extern void SerializeArrayMinus();
+            return wroteSize;
         }
     }
 }
