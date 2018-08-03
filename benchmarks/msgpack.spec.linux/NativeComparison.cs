7ea9328c93adba1897594d31289e81d0adfe8302
aensidhe <me@aensidhe.ru>
2018-08-03 11:47:33 +0300

    Effects of `baseInt-i` and direct call to formatting method.

--- benchmarks/msgpack.spec.linux/NativeComparison.cs
+++ benchmarks/msgpack.spec.linux/NativeComparison.cs
@@ -23,7 +23,25 @@ namespace msgpack.spec.linux
             var buffer = _buffer.AsSpan();
             var wroteSize = MsgPackSpec.WriteArray16Header(buffer, length);
             for (var i = 0u; i < length; i++)
+                wroteSize += MsgPackSpec.WriteUInt32(buffer.Slice(wroteSize), baseInt);
+        }
+
+        [Benchmark]
+        public void MsgPackSpecArrayMinus()
+        {
+            var buffer = _buffer.AsSpan();
+            var wroteSize = MsgPackSpec.WriteArray16Header(buffer, length);
+            for (var i = 0u; i < length; i++)
                 wroteSize += MsgPackSpec.WriteUInt32(buffer.Slice(wroteSize), baseInt - i);
+        }
+
+        [Benchmark]
+        public void MsgPackSpecArrayFixInt32()
+        {
+            var buffer = _buffer.AsSpan();
+            var wroteSize = MsgPackSpec.WriteArray16Header(buffer, length);
+            for (var i = 0u; i < length; i++)
+                wroteSize += MsgPackSpec.WriteFixUInt32(buffer.Slice(wroteSize), baseInt);
         }
 
         [Benchmark]
@@ -38,49 +56,6 @@ namespace msgpack.spec.linux
                     pointer[3 + 5 * i] = DataCodes.UInt32;
                     Unsafe.WriteUnaligned(ref pointer[3 + 5 * i + 1], BinaryPrimitives.ReverseEndianness(baseInt));
                 }
-            }
-        }
-
-        [Benchmark]
-        public void SpanBigEndian()
-        {
-            var pointer = _buffer.AsSpan();
-            var l = BinaryPrimitives.ReverseEndianness(length);
-            pointer[0] = DataCodes.Array16;
-            MemoryMarshal.Write(pointer.Slice(1), ref l);
-            var x = BinaryPrimitives.ReverseEndianness(baseInt);
-            for (var i = 0; i < length; i++)
-            {
-                pointer[3 + 5 * i] = DataCodes.UInt32;
-                MemoryMarshal.Write(pointer.Slice(3 + 5 * i + 1), ref x);
-            }
-        }
-
-        [Benchmark]
-        public void SpanBigEndianLength()
-        {
-            var pointer = _buffer.AsSpan();
-            var l = BinaryPrimitives.ReverseEndianness(length);
-            pointer[0] = DataCodes.Array16;
-            MemoryMarshal.Write(pointer.Slice(1, 2), ref l);
-            var x = BinaryPrimitives.ReverseEndianness(baseInt);
-            for (var i = 0; i < length; i++)
-            {
-                pointer[3 + 5 * i] = DataCodes.UInt32;
-                MemoryMarshal.Write(pointer.Slice(3 + 5 * i + 1), ref x);
-            }
-        }
-
-        [Benchmark]
-        public void SpanBigEndianBinaryPrimitive()
-        {
-            var pointer = _buffer.AsSpan();
-            pointer[0] = DataCodes.Array16;
-            BinaryPrimitives.WriteUInt16BigEndian(pointer.Slice(1), length);
-            for (var i = 0; i < length; i++)
-            {
-                pointer[3 + 5 * i] = DataCodes.UInt32;
-                BinaryPrimitives.WriteUInt32BigEndian(pointer.Slice(3 + 5 * i + 1), baseInt);
             }
         }
 
