c48e24d55482ddf8273c391f73b203a3e0934c06
Anatoly Popov <aensidhe@users.noreply.github.com>
2018-12-08 00:09:10 +0300

    feat(ReadOnlySequence): tests for nil

--- tests/msgpack.spec.tests/ReadOnlySequence/Null.cs
+++ tests/msgpack.spec.tests/ReadOnlySequence/Null.cs
@@ -6,10 +6,18 @@ namespace ProGaudi.MsgPack.Tests.ReadOnlySequence
     public sealed class Null
     {
         [Fact]
-        public void ReadNull()
+        public void SingleSegmentReadNull()
         {
             var buffer = new [] { DataCodes.Nil };
-            MsgPackSpec.ReadNil(buffer, out var readSize);
+            MsgPackSpec.ReadNil(buffer.ToSingleSegment(), out var readSize);
+            readSize.ShouldBe(buffer.Length);
+        }
+
+        [Fact]
+        public void MultipleSegmentsReadNull()
+        {
+            var buffer = new [] { DataCodes.Nil };
+            MsgPackSpec.ReadNil(buffer.ToMultipleSegments(), out var readSize);
             readSize.ShouldBe(buffer.Length);
         }
     }
