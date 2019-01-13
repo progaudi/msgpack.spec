fc4f1f2888a88263785a15aa6714005907754235
Anatoly Popov <aensidhe@users.noreply.github.com>
2019-01-13 11:48:47 +0300

    refactor(Tests): make ToSingleSegment and ToMultipleSegments generic

--- tests/msgpack.spec.tests/ReadOnlySequence/Extensions.cs
+++ tests/msgpack.spec.tests/ReadOnlySequence/Extensions.cs
@@ -7,34 +7,34 @@ namespace ProGaudi.MsgPack.Tests.ReadOnlySequence
     [DebuggerStepThrough]
     public static class Extensions
     {
-        public static ReadOnlySequence<byte> ToSingleSegment(this byte[] buffer) => new ReadOnlySequence<byte>(buffer);
+        public static ReadOnlySequence<T> ToSingleSegment<T>(this T[] buffer) => new ReadOnlySequence<T>(buffer);
 
-        public static ReadOnlySequence<byte> ToMultipleSegments(this byte[] buffer)
+        public static ReadOnlySequence<T> ToMultipleSegments<T>(this T[] buffer)
         {
             var length = buffer.Length;
-            if (length <= 1) return ZeroOrOneByteSequence();
+            if (length <= 1) return ZeroOrOneElementSequence();
 
             var memory = buffer.AsMemory();
-            var last = new Segment(memory.Slice(buffer.Length - 1), null, length - 1);
-            var lastEmpty = new Segment(ReadOnlyMemory<byte>.Empty, last, length - 1);
-            var medium = new Segment(memory.Slice(1, length - 2), lastEmpty, 1);
-            var firstEmpty = new Segment(ReadOnlyMemory<byte>.Empty, medium, 1);
-            var first = new Segment(memory.Slice(0, 1), firstEmpty, 0);
-            return new ReadOnlySequence<byte>(first, 0, last, last.Memory.Length);
+            var last = new Segment<T>(memory.Slice(buffer.Length - 1), null, length - 1);
+            var lastEmpty = new Segment<T>(ReadOnlyMemory<T>.Empty, last, length - 1);
+            var medium = new Segment<T>(memory.Slice(1, length - 2), lastEmpty, 1);
+            var firstEmpty = new Segment<T>(ReadOnlyMemory<T>.Empty, medium, 1);
+            var first = new Segment<T>(memory.Slice(0, 1), firstEmpty, 0);
+            return new ReadOnlySequence<T>(first, 0, last, last.Memory.Length);
 
-            ReadOnlySequence<byte> ZeroOrOneByteSequence()
+            ReadOnlySequence<T> ZeroOrOneElementSequence()
             {
-                var afterByte = new Segment(ReadOnlyMemory<byte>.Empty, null, length);
-                var @byte = new Segment(buffer, afterByte, 0);
-                var beforeByte = new Segment(ReadOnlyMemory<byte>.Empty, @byte, 0);
+                var endSegment = new Segment<T>(ReadOnlyMemory<T>.Empty, null, length);
+                var element = new Segment<T>(buffer, endSegment, 0);
+                var startSegment = new Segment<T>(ReadOnlyMemory<T>.Empty, element, 0);
 
-                return new ReadOnlySequence<byte>(beforeByte, 0, afterByte, 0);
+                return new ReadOnlySequence<T>(startSegment, 0, endSegment, 0);
             }
         }
 
-        private sealed class Segment : ReadOnlySequenceSegment<byte>
+        private sealed class Segment<T> : ReadOnlySequenceSegment<T>
         {
-            public Segment(ReadOnlyMemory<byte> memory, ReadOnlySequenceSegment<byte> next, long runningIndex)
+            public Segment(ReadOnlyMemory<T> memory, ReadOnlySequenceSegment<T> next, long runningIndex)
             {
                 Memory = memory;
                 Next = next;
